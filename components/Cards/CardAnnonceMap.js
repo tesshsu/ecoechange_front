47dddb42d427764b6b9ae5fb19664fec30833822
renzky15 <renzowensantillan@gmail.com>
2021-06-03 11:24:59 +0800

    added react-leaflet and openmapstreet

--- components/Cards/CardAnnonceMap.js
+++ components/Cards/CardAnnonceMap.js
@@ -1,63 +1,122 @@
-import React, { useState, useEffect } from "react";
-import {connect} from "react-redux";
-import GoogleMapReact from 'google-map-react';
+import React, { useState, useEffect, useRef } from "react"
+import { connect } from "react-redux"
+import GoogleMapReact from "google-map-react"
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import { Icon, marker, latLng } from "leaflet"
+import axios from "axios"
 
-const CustomMarker = ({ text }) => <div className="custom-marker rounded-full">
-	<img className="markerMap" src={require("assets/img/brand/apple-icon.png")} alt="marker"/>
-</div>;
+// const CustomMarker = ({ text }) => (
+//     <div className="custom-marker rounded-full">
+//         <img
+//             className="markerMap"
+//             src={require("assets/img/brand/apple-icon.png")}
+//             alt="marker"
+//         />
+//     </div>
+// )
 
+const CardAnnonceMap = ({ dispatch, loading, idea }) => {
+    const [lat, setLat] = useState()
+    const [lng, setLng] = useState()
+    const [address, setAddress] = useState()
+    const [LatLng, setLatLng] = useState()
+    const [map, setMap] = useState(null)
 
-const CardAnnonceMap = ({ dispatch,
-							loading,
-							idea}) => {
+    const markers = {
+        LatLng: [lat ? lat : 0, lng ? lng : 0],
+    }
 
-	const markers = [
-		{
-			name: idea?.title,
-			latlng: [52.499040, 13.418392]
-		}
-	];
+    useEffect(() => {
+        const fetchApi = async () => {
+            try {
+                const addressSplit = idea?.shop_address.split(" ")
+                const addressJoin = addressSplit.join("+")
 
-	const mapConfig = {
-		center: [52.499040, 13.418392],
-		zoom: 15
-	};
+                setTimeout(async () => {
+                    const res = await axios.get(
+                        `https://nominatim.openstreetmap.org/search?q=${addressJoin}&format=jsonv2&limit=1`
+                    )
 
-	const GoogleMapsMarkers = markers.map(marker => (
-		<CustomMarker
-			key={`marker_${marker.name}`}
-			lat={marker.latlng[0]}
-			lng={marker.latlng[1]}
-			text={marker.name}
-		/>
-	));
+                    try {
+                        setLat(parseFloat(res.data[0].lat))
+                        setLng(parseFloat(res.data[0].lon))
+                        console.log(
+                            parseFloat(res.data[0].lat),
+                            parseFloat(res.data[0].lon)
+                        )
 
-	return (
-		<>
-			{idea?.shop_address && (
-				<div style={{ height: '60vh', width: '100%' }}>
-					<GoogleMapReact
-						defaultCenter={mapConfig.center}
-						defaultZoom={mapConfig.zoom}
-						layerTypes={['TransitLayer', 'TransitLayer']}
-						bootstrapURLKeys={{
-							key: 'AIzaSyC2fW_aWDQHUasTrFoS9GlcJLraQnzCu_Q',
-							language: 'fr'
-						}}
-					>
-						{GoogleMapsMarkers}
-					</GoogleMapReact>
-				</div>
+                        if (map) {
+                            map.flyTo([
+                                parseFloat(res.data[0].lat),
+                                parseFloat(res.data[0].lon),
+                            ])
+                        }
+                    } catch (error) {
+                        console.log(error.message)
+                    }
 
-			)}
-		</>
-	);
+                    // setLatLng([res.data[0].lat, res.data[0].lon])
+                }, 1000)
+
+                console.log(lat, lng)
+                // if (map) {
+                //     map.flyTo([
+                //         parseFloat(res.data[0].lat),
+                //         parseFloat(res.data[0].lon),
+                //     ])
+                // }
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+        fetchApi()
+    }, [lat])
+
+    console.log(LatLng)
+
+    return (
+        <>
+            {idea?.shop_address ? (
+                <MapContainer
+                    center={[48.864716, 2.349014]}
+                    zoom={15}
+                    scrollWheelZoom={true}
+                    dragging={true}
+                    animate={true}
+                    style={{ height: "60vh", width: "100%" }}
+                    whenCreated={(map) => setMap(map)}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+
+                    <Marker
+                        position={markers.LatLng}
+                        icon={
+                            new Icon({
+                                iconUrl:
+                                    "https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png",
+                                iconSize: [25, 41],
+                                iconAnchor: [12, 41],
+                            })
+                        }
+                    >
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            ) : null}
+        </>
+    )
 }
 
 const mapStateToProps = (state) => ({
-	loading: state.ideasReducer.loading,
-	idea: state.ideasReducer.selectedIdea,
-	hasErrors: state.ideasReducer.hasErrors,
+    loading: state.ideasReducer.loading,
+    idea: state.ideasReducer.selectedIdea,
+    hasErrors: state.ideasReducer.hasErrors,
 })
 
 export default connect(mapStateToProps)(CardAnnonceMap)
--- components/Dropdowns/AnnonceDetails/DetailsDropdown.js
+++ components/Dropdowns/AnnonceDetails/DetailsDropdown.js
@@ -1,56 +1,67 @@
-import React from "react";
-import {connect} from "react-redux";
-import CardAnnonceMap from "../../Cards/CardAnnonceMap";
-//import CardAnnonceSlider from "../../Cards/CardAnnonceSlider";
-const DetailsDropdown = ({
-							 dispatch,
-							 loading,
-							 idea
-						 }) => {
+import React from "react"
+import { connect } from "react-redux"
+// import CardAnnonceMap from "../../Cards/CardAnnonceMap"
+import CardAnnonceSlider from "../../Cards/CardAnnonceSlider"
+import dynamic from "next/dynamic"
+const DetailsDropdown = ({ dispatch, loading, idea }) => {
+    const address_boutique = idea?.shop_address ? idea.shop_address : "none"
+    const ncs = [
+        {
+            icon: "fas fa-seedling",
+            name: "Détails de l’idée",
+            value: idea?.content,
+        },
+        {
+            icon: "fas fa-map-pin",
+            name: "Adresse de la boutique",
+            value: address_boutique,
+        },
+    ]
 
-    const address_boutique = idea?.shop_address ? idea.shop_address : "none";
-	const ncs = [
-		{icon: "fas fa-seedling", name: "Détails de l’idée", value: idea?.content},
-		{icon: "fas fa-map-pin", name: "Adresse de la boutique", value: address_boutique},
-	];
+    let Equips = idea?.equipments
 
-	let Equips = idea?.equipments
+    const Map = dynamic(
+        () => import("../../Cards/CardAnnonceMap"), // replace '@components/map' with your component's location
+        {
+            loading: () => <p>A map is loading</p>,
+            ssr: false, // This line is important. It's what prevents server-side render
+        }
+    )
 
-	return (
-		<>
-			<div className="flex flex-wrap">
-				<div className="w-full px-8 py-2 px-2 flex-1">
-					<h4 className="mt-2 px-2 py-2 text-xl leading-relaxed text-gray-800 underline font-bold uppercase rounded text-center">
-						<i class="fab fa-pagelines"></i> L’IDÉE EST LA...
-					</h4>
-				</div>
-				{ncs?.map(nc => (
-					<div className="container px-2 mx-auto">
-						<div className="flex flex-wrap">
-							<div className="w-full px-4">
-								<span
-									className="text-xl block my-2 p-3 text-gray-800 font-bold rounded border border-solid border-gray-200"><i
-									className={nc.icon}></i> {nc.name} </span>
-							</div>
-							<div className="w-full px-4">
-							    <span
-									className="ideaTextarea question-1 text-xl block my-2 p-3 text-green-500 rounded border border-solid border-gray-200">
-								  {nc.value}
-								</span>
-							</div>
-						</div>
-					</div>
-				))}
-				<CardAnnonceMap/>
-			</div>
-		</>
-	);
-};
+    return (
+        <>
+            <div className="flex flex-wrap">
+                <div className="w-full px-8 py-2 px-2 flex-1">
+                    <h4 className="mt-2 px-2 py-2 text-xl leading-relaxed text-gray-800 underline font-bold uppercase rounded text-center">
+                        <i className="fab fa-pagelines"></i> L’IDÉE EST LA...
+                    </h4>
+                </div>
+                {ncs?.map((nc) => (
+                    <div className="container px-2 mx-auto">
+                        <div className="flex flex-wrap">
+                            <div className="w-full px-4">
+                                <span className="text-xl block my-2 p-3 text-gray-800 font-bold rounded border border-solid border-gray-200">
+                                    <i className={nc.icon}></i> {nc.name}{" "}
+                                </span>
+                            </div>
+                            <div className="w-full px-4">
+                                <span className="ideaTextarea question-1 text-xl block my-2 p-3 text-green-500 rounded border border-solid border-gray-200">
+                                    {nc.value}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                <Map />
+            </div>
+        </>
+    )
+}
 
 const mapStateToProps = (state) => ({
-	loading: state.ideasReducer.loading,
-	idea: state.ideasReducer.selectedIdea,
-	hasErrors: state.ideasReducer.hasErrors,
+    loading: state.ideasReducer.loading,
+    idea: state.ideasReducer.selectedIdea,
+    hasErrors: state.ideasReducer.hasErrors,
 })
 
 export default connect(mapStateToProps)(DetailsDropdown)
